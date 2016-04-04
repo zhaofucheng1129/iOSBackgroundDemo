3827608bf26a88915f1ff9f1b37327350be1ae63
Tommy <zhaofucheng1129@gmail.com>
2016-04-04 12:52:39 +0800

    Initial Commit

---
+++ iOSBackgroundDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSBackgroundDemo
+//
+//  Created by ZhaoFucheng on 16/4/4.
+//  Copyright © 2016年 ZhaoFucheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
